3804ffde35a72c50968f26332ea8dd5b66fca30b
Kay Lukas <kay.lukas@gmail.com>
2017-03-20 10:33:41 +0100

    Added examples

---
+++ examples/config.template.php
@@ -0,0 +1,10 @@
+<?php
+return [/*'api_url' => '', The URL to the API, only for development purposes. */
+        'username' => 'brand@example.com',//Your brand email/username
+        'password' => 'password', //Clear text password.
+            //some variables that can be used for testing. The content depends on your brand set up.
+        'company_without_access' => 20,
+        'company_accessible' => 45,
+        'order_without_access' => '4',
+        'order_with_access' => '30'
+];
